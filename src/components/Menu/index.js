7d91412b5b4118243e7f6f43b7945872c11d0b09
Guilherme <guilhermebromonschenkel@gmail.com>
2019-08-17 17:37:13 -0300

    Added config elements when scrolling the card

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,12 +1,37 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
 import QRCode from '~/assets/QRCode.png'
 
-import { Container, Code } from './styles'
+import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles'
 
 export default function Menu(){
     return (
         <Container>
             <Code source={QRCode} />
+
+            <Nav>
+                <NavItem>
+                    <Icon name="help-outline" size={20} color="#FFF" />
+                    <NavText>Me ajuda</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="person-outline" size={20} color="#FFF" />
+                    <NavText>Perfil</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="credit-card" size={20} color="#FFF" />
+                    <NavText>Configurar cartao</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="smartphone" size={20} color="#FFF" />
+                    <NavText>Configurações do app</NavText>
+                </NavItem>
+            </Nav>
+
+            <SignOutButton onPress={() => {}}>
+                <SignOutButtonText>SAIR DO APP</SignOutButtonText>
+            </SignOutButton>
         </Container>
     )
 }
