8d71d8b88ccd7efb71f34d00186e454213ed41bd
Guilherme <guilhermebromonschenkel@gmail.com>
2019-08-17 14:47:24 -0300

    Added tabs style and elements

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import { Container } from './styles'
+import { Container, Top, Logo, Title } from './styles'
+
+import logo from '~/assets/Nubank_Logo.png'
 
 export default function Header(){
     return (
-        <Container />
+        <Container>
+            <Top>
+                <Logo source={logo} />
+                <Title>Guilherme</Title>
+            </Top>
+            <Icon name="keyboard-arrow-down" size={20} color="#FFF" />
+        </Container>
     )
 }
